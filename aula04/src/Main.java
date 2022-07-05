ab87783c012c5da8c95a8d73b12cfcb60ffe0650
Christian Mesaque <you@example.com>
2022-07-05 14:47:55 -0300

    update

---
+++ aula04/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta04 c1 = new Caneta04("BIC", "Azul", 0.5f);
+        c1.status();
+        Caneta04 c2 = new Caneta04("BIC", "Vermelha", 0.5f);
+        c2.status();
+    }
+}
